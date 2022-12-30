c667f600021de8cb3be6c4631ddd6d3baf2b69ca
frlyh <liyaohua_it@163.com>
2022-12-30 10:33:06 +0800

    init

--- finance/PaymentSplitter_v1.sol
+++ finance/PaymentSplitter_v1.sol
@@ -1,7 +1,16 @@
-pragma solidity ^0.4.0;
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-contract PaymentSplitter_v1 {
-    function PaymentSplitter_v1(){
+import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
+
+contract PaymentSplitter_v1 is PaymentSplitter {
+
+    address[] private payees = [0x04C0ab5684EAEBecf1aCCe32c0c5a1B07425EB80, 0x0CFB60aa24eE5606CECBAF709801b0Ef78088E5B];
+    uint256[] private shares_ = [1, 2];
+
+    constructor() PaymentSplitter(payees, shares_) {
 
     }
+
 }
--- finance/VestingWallet_v1.sol
+++ finance/VestingWallet_v1.sol
@@ -1,7 +1,28 @@
-pragma solidity ^0.4.0;
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-contract VestingWallet_v1 {
-    function VestingWallet_v1(){
+import "@openzeppelin/contracts/finance/VestingWallet.sol";
+
+contract VestingWallet_v1 is VestingWallet{
+    constructor(address beneficiaryAddress, uint64 durationSeconds) VestingWallet(beneficiaryAddress, uint64(block.timestamp), uint64(durationSeconds)) {
 
     }
+    function releaseToken() public virtual{
+        require(checkExpiry(),"Time is not up");
+        release();
+
+    }
+    function checkExpiry() public view returns(bool success){
+        uint256 time = VestingWallet.start() + VestingWallet.duration();
+
+        if(time > uint256(block.timestamp)){
+
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
 }
